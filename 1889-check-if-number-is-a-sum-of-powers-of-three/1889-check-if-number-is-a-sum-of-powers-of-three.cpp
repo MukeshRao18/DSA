293989a2432b27578d3d91cdcf26438c06570f71
Beerkoor Mukesh Kumar <138296745+MukeshRao18@users.noreply.github.com>
2025-03-04 23:02:59 +0530

    Time: 0 ms (100%), Space: 8 MB (19.48%) - LeetHub

--- 1889-check-if-number-is-a-sum-of-powers-of-three/1889-check-if-number-is-a-sum-of-powers-of-three.cpp
+++ 1889-check-if-number-is-a-sum-of-powers-of-three/1889-check-if-number-is-a-sum-of-powers-of-three.cpp
@@ -1,12 +1,25 @@
+#include <cmath>  // for pow()
+
 class Solution {
 public:
     bool checkPowersOfThree(int n) {
-        while (n > 0) {
-            if (n % 3 == 2) {
-                return false;
-            }
-            n /= 3;
+        int i = 0;
+        // Find the largest power of 3 that is ≤ n
+        while (pow(3, i) <= n) {
+            i++;
         }
-        return true;
+        i--;  // Step back to the last valid power
+
+        // Try to subtract powers of 3 from n
+        while (i >= 0 && n > 0) {
+            int power = pow(3, i);
+            if (n >= power) {
+                n -= power;
+            }
+            i--;
+        }
+
+        // If n is reduced to zero, n can be represented as sum of powers of 3
+        return n == 0;
     }
 };
